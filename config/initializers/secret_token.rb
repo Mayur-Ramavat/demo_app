0a7aef3a365a9b8ef5491b735f2be73a4083fc49
MAYUR <mayur@farematrix.com>
2012-12-31 15:50:04 +0530

    adding all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1a0511908c74e51329da8a61f117025811c8c418251da870df1cb60d22a54e89db104d7d9d3d20f77ad8616ec25a1a5c6f42858e48c11602c145f9abd34dfe21'
